12ed7ebc7c4cf8db68b03049c36f68e2546e1ebb
Dylan Meunier <dylan19170@gmail.com>
2023-06-08 23:27:12 +0200

    first setup and playbook test

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install required packages
+sudo apt-get update
+sudo apt-get install -y curl git python3 python3-pip python3-setuptools
+
+# Install Ansible
+sudo apt-get update
+sudo apt-get install -y ansible
+
+# Download the playbook from the GitHub repository
+curl -L https://raw.githubusercontent.com/paysancorrezien/randomstuff/main/debian.yml --output playbook.yml
+
+# Execute the playbook
+ansible-playbook playbook.yml
